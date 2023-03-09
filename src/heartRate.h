e5f46dde8d4d30fadb1814babf8e1bf4caa862ab
Nitin Nair <nitin7nair@gmail.com>
2023-03-08 20:57:12 -0500

    updated function signature for heart rate to return intermediates

--- src/heartRate.h
+++ src/heartRate.h
@@ -61,7 +61,7 @@
  #include "WProgram.h"
 #endif
 
-bool checkForBeat(int32_t sample);
+bool checkForBeat(int32_t sample, int16_t &dcEst, int16_t &dcRemoved, int16_t &firFiltData);
 int16_t averageDCEstimator(int32_t *p, uint16_t x);
 int16_t lowPassFIRFilter(int16_t din);
 int32_t mul16(int16_t x, int16_t y);
